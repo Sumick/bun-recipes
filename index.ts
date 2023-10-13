72694e2a00100660da4a891e6667ec5ac3afee3f
Jakub Wąsowski <jakub@nerdz.pl>
2023-10-13 16:20:35 +0200

    csr

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 const server = Bun.serve({
-    port: Bun.env.PORT || 3000,
-    fetch(req) {
-        return new Response("Nice bun!");
-    },
+  port: Bun.env.PORT || 3000,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/buns") {
+      const buns = Bun.file("./data.json");
+      const bunResponse = new Response(buns);
+
+      bunResponse.headers.set("Access-Control-Allow-Origin", "*");
+
+      return bunResponse;
+    }
+    return new Response("Nice bun!");
+  },
 });
 
-console.log(`Server is running on port ${server.port}`)
+console.log(`Server is running on port ${server.port}`);
