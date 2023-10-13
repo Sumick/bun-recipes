10ecc14b7498fcc4faeb65115a3d4e2fe5bcdb7f
Jakub Wąsowski <jakub@nerdz.pl>
2023-10-13 16:32:04 +0200

    ssr

---
+++ index.tsx
@@ -0,0 +1,30 @@
+import { renderToReadableStream } from "react-dom/server";
+import { BunsList } from "./src/BunsList.tsx";
+import { RecipeList } from "./src/types.ts";
+
+const server = Bun.serve({
+  port: Bun.env.PORT || 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/app") {
+      const buns = (await Bun.file("./data.json").json()) as RecipeList;
+      const stream = await renderToReadableStream(<BunsList buns={buns} />);
+      return new Response(stream, {
+        headers: { "Content-Type": "text/html" },
+      });
+    }
+
+    if (url.pathname === "/buns") {
+      const buns = Bun.file("./data.json");
+      const bunResponse = new Response(buns);
+
+      bunResponse.headers.set("Access-Control-Allow-Origin", "*");
+
+      return bunResponse;
+    }
+    return new Response("Nice bun!");
+  },
+});
+
+console.log(`Server is running on port ${server.port}`);
